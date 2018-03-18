e1a2381ab87b2075751ae0142bdd7bda519e7b5c
YusukeKato <s1526035cn@s.chibakoudai.jp>
2018-03-19 00:32:56 +0900

    Add test script

---
+++ Test_Script/test.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2018 Yusuke Kato All Rights Reserved. 
+// License : MIT
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    float point = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 NewPosition = transform.position;
+        if(NewPosition.x > 1.0f)
+        {
+            point = -0.01f;
+        }
+        else if(NewPosition.x < -1.0f)
+        {
+            point = 0.01f;
+        }
+        NewPosition.x += point;
+        transform.position = NewPosition;
+	}
+}
