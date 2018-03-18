cd5d786a69bf31b9e09413ea593ff60eb352bfd5
YusukeKato <s1526035cn@s.chibakoudai.jp>
2018-03-19 00:38:09 +0900

    edit indent

--- Test_Script/test.cs
+++ Test_Script/test.cs
@@ -5,27 +5,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour {
+public class test : MonoBehaviour
+{
 
     float point = 0.01f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector3 NewPosition = transform.position;
-        if(NewPosition.x > 1.0f)
+        if (NewPosition.x > 1.0f)
         {
             point = -0.01f;
         }
-        else if(NewPosition.x < -1.0f)
+        else if (NewPosition.x < -1.0f)
         {
             point = 0.01f;
         }
         NewPosition.x += point;
         transform.position = NewPosition;
-	}
+    }
 }
